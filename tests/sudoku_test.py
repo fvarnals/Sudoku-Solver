bff23a869748fb4417323729a712299349b60b62
Freddie <f.varnals@gmail.com>
2019-10-01 21:50:06 +0100

    box object has associated index

--- tests/sudoku_test.py
+++ tests/sudoku_test.py
@@ -5,5 +5,6 @@ sys.path.insert(0, myPath + '/../lib')
 from box import Box
 box = Box((1,1))
 
-def test_check():
+def test_box_index():
     assert box.index == (1,1)
+    # assert box.possibilities == []
