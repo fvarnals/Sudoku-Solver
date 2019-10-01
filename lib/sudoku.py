d1765448ca2c23bd3c8dc3dce9c451064ca0c7f3
Freddie <f.varnals@gmail.com>
2019-10-01 22:18:02 +0100

    sudoku object contains box object

---
+++ lib/sudoku.py
@@ -0,0 +1,6 @@
+from box import Box
+
+class Sudoku:
+
+    def __init__(self):
+        self.box = Box((1,2))
--- tests/sudoku_test.py
+++ tests/sudoku_test.py
@@ -3,8 +3,13 @@ myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../lib')
 
 from box import Box
-box = Box((1,1))
+from sudoku import Sudoku
+box = Box((2,2))
 
 def test_box_index():
-    assert box.index == (1,1)
+    assert box.index == (2,2)
     # assert box.possibilities == []
+
+sudoku = Sudoku()
+def test_sudoku_has_box():
+    assert sudoku.box.index == (1,2)
