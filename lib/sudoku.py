f69b8543fffb7c6eeb105185189705289373285f
Freddie <f.varnals@gmail.com>
2019-10-01 22:26:21 +0100

    Sudoku holds box object for each number coordinate

--- lib/sudoku.py
+++ lib/sudoku.py
@@ -2,5 +2,8 @@ from box import Box
 
 class Sudoku:
 
-    def __init__(self):
-        self.box = Box((1,2))
+    def __init__(self,size=9):
+        self.boxes = {}
+        for x_coord in range(1,size+1):
+            for y_coord in range(1,size+1):
+                self.boxes[(x_coord,y_coord)] = Box((x_coord,y_coord))
--- tests/sudoku_test.py
+++ tests/sudoku_test.py
@@ -10,6 +10,7 @@ def test_box_index():
     assert box.index == (2,2)
     # assert box.possibilities == []
 
-sudoku = Sudoku()
+sudoku = Sudoku(9)
 def test_sudoku_has_box():
-    assert sudoku.box.index == (1,2)
+    assert sudoku.boxes[(1,1)].index == (1,1)
+    assert sudoku.boxes[(3,7)].index == (3,7)
